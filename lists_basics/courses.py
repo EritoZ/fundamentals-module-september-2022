69e1b156082712ab44e2655857f15840c34d672e
EritoZ <93884368+EritoZ@users.noreply.github.com>
2022-10-21 16:42:19 +0300

    Delete lists_basics directory

--- lists_basics/courses.py
+++
@@ -1,8 +0,0 @@
-lines = int(input())
-lst = []
-
-for i in range(lines):
-    course = input()
-    lst.append(course)
-
-print(lst)
--- lists_basics/invert_values.py
+++
@@ -1,7 +0,0 @@
-string = input()
-string = string.split(" ")
-
-for i, number in enumerate(string):
-    string[i] = int(number) * -1
-
-print(string)
