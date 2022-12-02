b912163542e7f3c7bff6b9b0ce19898d04e3db15
EritoZ <moni.grozdev@gmail.com>
2022-12-02 23:40:14 +0200

    Delete archives/strings_and_regular_expressions directory

--- archives/strings_and_regular_expressions/censorship.py
+++
@@ -1,6 +0,0 @@
-word = input()
-sentence = input()
-
-censored_sentence = sentence.replace(word, "*" * len(word))
-
-print(censored_sentence)
--- archives/strings_and_regular_expressions/only_letters.py
+++
@@ -1,9 +0,0 @@
-import re
-
-text = input()
-
-text = re.sub(r"\d+(?=[A-Za-z])", " ", text)
-while " " in text:
-    text = text.replace(" ", text[text.index(" ") + 1], 1)
-
-print(text)
