c4194fe0af348d3562e4c027619c836f604e721f
EritoZ <moni.grozdev@gmail.com>
2022-12-08 18:26:00 +0200

    Add files via upload

---
+++ archives/strings_and_text_processing/melrah_shake.py
@@ -0,0 +1,21 @@
+a_string = input()
+pattern = input()
+
+while a_string.count(pattern) >= 2 and pattern:
+
+    index_first_occurrence = a_string.index(pattern)
+
+    a_string = a_string[:index_first_occurrence] + a_string[index_first_occurrence + len(pattern):]
+
+    index_second_occurrence = a_string.rfind(pattern)
+
+    a_string = a_string[:index_second_occurrence] + a_string[index_second_occurrence + len(pattern):]
+
+    index_remove = len(pattern) // 2
+    pattern = pattern[:index_remove] + pattern[index_remove + 1:]
+
+    print("Shaked it.")
+
+print("No shake.")
+print(a_string)
+
